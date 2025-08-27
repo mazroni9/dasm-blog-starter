8399709b25a0f0d7c70a4d65d81ffe4af8b89027
DASM-e <zahrma0p@yahoo.com>
2025-08-27 23:03:44 +0300

    Update next.config.js
    اصلاح الملف بالكامل

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { mdxRs: false },
+  webpack: (config) => {
+    // نضيف alias عشان يشتغل الاستيراد بـ "@/lib/..."
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+  experimental: {
+    mdxRs: false, // لو ما تستخدم MDX
+  },
 };
+
 module.exports = nextConfig;
